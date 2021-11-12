61554d271f830243bd1b37beba07d3e56cf7735d
Rizwan Ahmad <rizahmad365@gmail.com>
2021-11-12 12:16:00 +0545

    Completed the UI, going up to contexts and global states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./Header";
+import Balance from "./Balance";
+import IncomeExpenses from "./IncomeExpenses";
+import TransactionList from "./TransactionList";
+import AddTransaction from "./AddTransaction";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
